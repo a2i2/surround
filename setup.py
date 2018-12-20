87f69a61efdcad44a77924e809e498fcc56f8451
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2018-12-20 12:08:00 +1100

    CONFIG: Collect package version from git tag

--- setup.py
+++ setup.py
@@ -2,27 +2,12 @@
 Python package definition for Surround
 """
 import os
-import sys
 
 from setuptools import setup, find_packages
-from setuptools.command.install import install
 
 
-# circleci.py version
-VERSION = "0.1.0"
-
-class VerifyVersionCommand(install):
-    """Custom command to verify that the git tag matches our version"""
-    description = 'Verify that the git tag matches our version'
-
-    def run(self):
-        tag = os.getenv('VERSION_TAG')
-
-        if tag != VERSION:
-            info = "Git tag: {0} does not match the version of this app: {1}".format(
-                tag, VERSION
-            )
-            sys.exit(info)
+# Collect version from repo tag
+VERSION = os.getenv('VERSION_TAG')
 
 setup(name='surround',
       version=VERSION,
@@ -32,8 +17,5 @@ setup(name='surround',
       author_email='scott.barnett@deakin.edu.au',
       packages=['surround'],
       test_suite="surround.tests",
-      zip_safe=False,
-      cmdclass={
-        'verify': VerifyVersionCommand,
-      }
+      zip_safe=False
 )
