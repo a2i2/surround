52aaaf73dabcba9e3e401a5b8bdde6225774618f
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2018-12-21 14:37:56 +1100

    NEW: Add web service runner

--- surround/__init__.py
+++ surround/__init__.py
@@ -1,3 +1,9 @@
 from .pipeline import Pipeline, PipelineData
 from .stage import Stage
 from .file_system_adapter import FileSystemAdapter
+try:
+    import flask
+    from .web_service_runner import WebServiceRunner
+except ImportError as error:
+    print("WARNING:", error)
+    pass
---
+++ surround/web_service_runner.py
@@ -0,0 +1,73 @@
+from .pipeline import PipelineData
+from flask import Flask, jsonify, request
+import abc
+import sys
+
+# Python 2.7 and 3.5 compatible classes:
+ABC = abc.ABCMeta('ABC', (object,), {'__slots__': ()})
+
+
+class TestData(PipelineData):
+    input = None
+    output = None
+
+class MetadataAction(object):
+    def __init__(self, metadata):
+        self.metadata = metadata
+
+    def __call__(self, *args):
+        return jsonify(self.metadata)
+
+class PredictAction(object):
+    methods = ['POST']
+
+    def __init__(self, pipeline, metadata):
+        self.metadata = metadata
+        self.pipeline = pipeline
+
+    def __call__(self, *args):
+        response = {}
+        if (request.is_json):
+            data = TestData()
+            data.input = request.get_json()
+            self.pipeline.process(data)
+            response = {
+                "output": data.output,
+                "version": self.metadata['version']
+            }
+        else:
+            data.input = request.json
+            response = {
+                "error": "Input is not a valid JSON"
+            }
+
+        return jsonify(response)
+
+class WebServiceRunner(ABC):
+    app = None
+
+    def __init__(self, pipeline, metadata=None):
+        self.app = Flask(__name__)
+
+        assert (metadata != None), "Metada is required"
+        try:
+            metadata['input']
+        except KeyError:
+            print('AssertionError: Missing "input" information in metadata')
+            sys.exit(1)
+        try:
+            metadata['output']
+        except KeyError:
+            print('AssertionError: Missing "output" information in metadata')
+            sys.exit(1)
+        try:
+            metadata['version']
+        except KeyError:
+            print('AssertionError: Missing "version" information in metadata')
+            sys.exit(1)
+
+        self.app.add_url_rule('/metadata', 'metadata', MetadataAction(metadata))
+        self.app.add_url_rule('/predict', 'predict', PredictAction(pipeline, metadata))
+
+    def start(self):
+        self.app.run(debug=True)
