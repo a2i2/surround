913d8d43d6dc016e3350092b875be9283c6162ee
AkshatBajaj <akshat.bajaj@outlook.com>
2019-01-08 15:41:33 +1100

    ENHANCE: Return stage output from operate

--- surround/stage.py
+++ surround/stage.py
@@ -19,8 +19,10 @@ class Stage(ABC):
     def operate(self, pipeline_data, config=None):
         """
         A stage in a pipeline.
-        :param pipeline_data: Instance or child of the PipelineData class.
-                              Stores intermediate data from each stage in the pipeline.
-        :param config: A ConfigParser object that contains the settings for each stage.
+        :param pipeline_data: Stores intermediate data from each stage in the pipeline.
+        :type pipeline_data: Instance or child of the PipelineData class
+        :param config: Contains the settings for each stage.
+        :type config: ConfigParser object
+        :returns: stage output
         """
         pass
