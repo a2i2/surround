49602ddc27d71f182bd995a3458aa2de48098740
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2019-01-11 08:28:25 +1100

    FIX: Use not is to check for None

--- examples/file-adapter/main.py
+++ examples/file-adapter/main.py
@@ -2,5 +2,5 @@ import subprocess
 
 process = subprocess.Popen(['python', 'examples/file-adapter/file_adapter.py', '-f0=examples/file-adapter/data/input.csv', '-c=examples/file-adapter/config.yaml', '-o', 'examples/file-adapter/data/'])
 process.wait()
-if process.returncode != None and process.returncode > 0:
+if not (process.returncode is None) and process.returncode > 0:
     raise Exception('Failed to run sampe file adapter app. Error code: ' + str(process.returncode))
--- examples/web-server/main.py
+++ examples/web-server/main.py
@@ -6,5 +6,5 @@ process = subprocess.Popen(['python', 'examples/web-server/sample_web_service.py
 time.sleep(5)
 process.terminate()
 process.poll()
-if process.returncode != None and process.returncode > 0:
+if not (process.returncode is None) and process.returncode > 0:
     raise Exception('Failed to run sampe web service app. Error code: ' + str(process.returncode))
