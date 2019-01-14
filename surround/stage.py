19866463f9358fdc09a7df67cba3079c15ff9c4e
AkshatBajaj <akshat.bajaj@outlook.com>
2019-01-14 13:54:40 +1100

    NEW: Create dump output method in stage

--- surround/stage.py
+++ surround/stage.py
@@ -2,6 +2,16 @@ from abc import ABC, abstractmethod
 
 
 class Stage(ABC):
+    def dump_output(self, output_dir, output, config):
+        """Dump the output of each stage.
+
+        :param output_dir: Output directory
+        :type output_dir :str
+        :param output: Output to dump
+        :type output: Same as surround_data (depends on implementation)
+        :param config: Config of the pipeline
+        :type config: <class 'surround.config.Config'>
+        """
 
     @abstractmethod
     def operate(self, surround_data, config):
