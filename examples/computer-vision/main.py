e334280bc04a4ffd5ba03657d881c55b0ebc5d9d
Luis Torres <luis.torresandrade@deakin.edu.au>
2019-01-22 14:50:52 +1100

    TEST: Add template for computer vision example

---
+++ examples/computer-vision/main.py
@@ -0,0 +1,10 @@
+# template script to implement a computer vision feature via surround
+import logging
+
+
+def main():
+    logging.info("This is the main function ...")
+
+
+if __name__ == '__main__':
+    main()
