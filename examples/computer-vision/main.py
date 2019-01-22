09dd8ca741b96f130017d9aa2ac0411b55632ec1
Luis Torres <luis.torresandrade@deakin.edu.au>
2019-01-22 15:48:14 +1100

    NEW: Add template to generate video stream

--- examples/computer-vision/main.py
+++ examples/computer-vision/main.py
@@ -1,9 +1,22 @@
 # template script to implement a computer vision feature via surround
 import logging
+import cv2
 
 
 def main():
     logging.info("This is the main function ...")
+    # skeleton for video stream
+    video_stream = cv2.VideoCapture(0)
+    # iteration to grab frames from the video
+    while True:
+        # grab the frame array
+        boolean_flag, frame_img_bgr = video_stream.read()
+        # show the frame
+        cv2.imshow('Frame', frame_img_bgr)
+        # stop the video stream via the keyboard: "q"
+        keyboard_event_stop = cv2.waitKey(1) & 0xFF
+        if keyboard_event_stop == ord("q"):
+            break
 
 
 if __name__ == '__main__':
