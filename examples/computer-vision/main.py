b42ebdab634ad86be89a504b71e475323948d625
Luis Torres <luis.torresandrade@deakin.edu.au>
2019-01-22 16:52:00 +1100

    NEW: Simulate face location inference

--- examples/computer-vision/main.py
+++ examples/computer-vision/main.py
@@ -1,6 +1,14 @@
 # template script to implement a computer vision feature via surround
 import logging
 import cv2
+import face_recognition as fr
+
+
+# function(s) to pre-process
+def bgr_to_rgb(bgr_img_array):
+    # convert BGR --> RGB space
+    rgb_img_array = cv2.cvtColor(bgr_img_array, cv2.COLOR_BGR2RGB)
+    return rgb_img_array
 
 
 # generic template to apply inference on an image array
@@ -11,7 +19,9 @@ def inference_frame(img_array, config_settings=None):
     :param config_settings:
     :return: Inference information
     """
-    return None
+    # replicate the inference of a face location algorithm
+    face_coords = fr.face_locations(img_array)
+    return face_coords
 
 
 def main():
@@ -22,10 +32,17 @@ def main():
     while True:
         # grab the frame array
         boolean_flag, frame_img_bgr = video_stream.read()
-        # apply inference on the frame
-        modified_frame = inference_frame(frame_img_bgr)
-        # show the frame
-        cv2.imshow('Frame', frame_img_bgr)
+        # pre-process the frame - rescale
+        frame_img_bgr_resized = cv2.resize(frame_img_bgr, None, fx=0.5, fy=0.5)
+        # map to rgb space
+        frame_img_rgb = bgr_to_rgb(frame_img_bgr_resized)
+        # apply face location
+        face_locations = fr.face_locations(frame_img_rgb)
+        # visual display of faces bounding boxes
+        for (y1, x2, y2, x1) in face_locations:
+            cv2.rectangle(frame_img_bgr_resized, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        # show the processed frame
+        cv2.imshow('Frame', frame_img_bgr_resized)
         # stop the video stream via the keyboard: "q"
         keyboard_event_stop = cv2.waitKey(1) & 0xFF
         if keyboard_event_stop == ord("q"):
