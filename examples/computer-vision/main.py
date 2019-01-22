9b8b8b06a30696a8c2f479fbc63f17cd37143907
Luis Torres <luis.torresandrade@deakin.edu.au>
2019-01-22 15:58:06 +1100

    NEW: Add template for inference

--- examples/computer-vision/main.py
+++ examples/computer-vision/main.py
@@ -3,6 +3,17 @@ import logging
 import cv2
 
 
+# generic template to apply inference on an image array
+def inference_frame(img_array, config_settings=None):
+    """
+    Do something with the `img_array'
+    :param img_array:
+    :param config_settings:
+    :return: Inference information
+    """
+    return None
+
+
 def main():
     logging.info("This is the main function ...")
     # skeleton for video stream
@@ -11,6 +22,8 @@ def main():
     while True:
         # grab the frame array
         boolean_flag, frame_img_bgr = video_stream.read()
+        # apply inference on the frame
+        modified_frame = inference_frame(frame_img_bgr)
         # show the frame
         cv2.imshow('Frame', frame_img_bgr)
         # stop the video stream via the keyboard: "q"
