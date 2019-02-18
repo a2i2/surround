dfb40001e2a46d1cd348ae25dd3f2159bf854db7
AkshatBajaj <akshat.bajaj@outlook.com>
2019-02-18 14:39:05 +1100

    DOC: Fix docstring of write_remote_to_file

--- surround/remote/base.py
+++ surround/remote/base.py
@@ -10,12 +10,15 @@ __date__ = '2019/02/18'
 
 class BaseRemote(ABC):
     def write_remote_to_file(self, file_, name, path):
-        '''Write remote to a file
+        """Write remote to a file
 
-        @param {str} file_: file to write
-        @param {str} name: name of the remote
-        @param {str} path: path to the remote
-        '''
+        :param file_: file to write
+        :type file_: str
+        :param name: name of the remote
+        :type name: str
+        :param path: path to the remote
+        :type path: str
+        """
         # Make directory if not exists
         os.makedirs(os.path.dirname(file_), exist_ok=True)
 
