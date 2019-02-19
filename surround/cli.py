7fdf47397fde6f520f612e87a6238d6ef46ed2d1
AkshatBajaj <akshat.bajaj@outlook.com>
2019-02-19 16:18:40 +1100

    NEW: Ask for remote while push and pull

--- surround/cli.py
+++ surround/cli.py
@@ -196,14 +196,17 @@ def main():
 
     # pull
     pull_parser = sub_parser.add_parser('pull', help="Pull the data from remote")
+    pull_parser.add_argument('-r', '--remote', help="remote to pull")
     pull_parser.add_argument('-f', '--file', help="file to pull(from .surround/config.yaml)")
 
     # push
     push_parser = sub_parser.add_parser('push', help="Push data to remote")
+    push_parser.add_argument('-r', '--remote', help="remote to push")
     push_parser.add_argument('-f', '--file', help="file to push (from .surround/config.yaml)")
 
     # Local Remote
     local = Local()
+    base_class = base.BaseRemote()
 
     # Check for valid sub commands as 'add_subparsers' in Python < 3.7
     # is missing the 'required' keyword
