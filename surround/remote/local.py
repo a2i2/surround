8a368212cb0de8786698debad4aaf8148d5b857a
AkshatBajaj <akshat.bajaj@outlook.com>
2019-02-20 15:42:39 +1100

    FIX: Use generic read from config in push and pull

--- surround/remote/local.py
+++ surround/remote/local.py
@@ -10,10 +10,25 @@ class Local(BaseRemote):
             name = self.get_file_name(file_)
             f.write(name + ": " + file_ + "\n")
 
-    def pull(self, file_=None):
-        file_to_pull = self.read_data_from_surround_local_config(file_)
-        copyfile(file_to_pull, 'data/input/' + file_)
+    def pull(self, what_to_pull, file_=None):
+        if file_:
+            file_to_pull = self.read_from_config(what_to_pull, file_)
+            if file_to_pull:
+                copyfile(file_to_pull, 'data/input/' + file_)  
+            else:
+                print("File not added")
+                print("Add that by surround add")
+        else:
+            pass
 
-    def push(self, file_=None):
-        file_to_push = self.read_data_from_surround_local_config(file_)
-        copyfile('data/input/' + file_, file_to_push)
+
+    def push(self, what_to_push, file_=None):
+        if file_:
+            file_to_push = self.read_from_config(what_to_push, file_)
+            if file_to_push:
+                copyfile('data/input/' + file_, file_to_push)
+            else:
+                print("File not added")
+                print("Add that by surround add")
+        else:
+            pass
