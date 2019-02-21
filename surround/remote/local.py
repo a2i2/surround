323adbd0744ac2c247fca410460679260b43e143
AkshatBajaj <akshat.bajaj@outlook.com>
2019-02-21 12:41:27 +1100

    NEW: Check file exists in cli add

--- surround/remote/local.py
+++ surround/remote/local.py
@@ -8,29 +8,34 @@ __date__ = '2019/02/18'
 
 class Local(BaseRemote):
     def add(self, add_to, file_):
-        name = self.get_file_name(file_)
+        # Check file is valid
+        f = Path(file_)
+        if f.is_file():
+            name = self.get_file_name(file_)
 
-        with open(".surround/config.yaml", "r") as f:
-            read_config = yaml.load(f) or {}
+            with open(".surround/config.yaml", "r") as f:
+                read_config = yaml.load(f) or {}
 
-        if "remote" in read_config and add_to in read_config["remote"]:
-            # Append filename
-            path_to_file = read_config["remote"][add_to] + "/" + name
-            self.write_config(add_to, ".surround/config.yaml", name, path_to_file)
-            return "File added successfully"
-        else:
-            home = str(Path.home())
+            if "remote" in read_config and add_to in read_config["remote"]:
+                # Append filename
+                path_to_file = read_config["remote"][add_to] + "/" + name
+                self.write_config(add_to, ".surround/config.yaml", name, path_to_file)
+                return "File added successfully"
+            else:
+                home = str(Path.home())
 
-            if Path(home + "/.surround/config.yaml").exists():
-                if "remote" in read_config and add_to in read_config["remote"]:
-                    # Append filename
-                    path_to_file = read_config["remote"][add_to] + "/" + name
-                    self.write_config(add_to, home + "/.surround/config.yaml", name, path_to_file)
-                    return "File added successfully"
+                if Path(home + "/.surround/config.yaml").exists():
+                    if "remote" in read_config and add_to in read_config["remote"]:
+                        # Append filename
+                        path_to_file = read_config["remote"][add_to] + "/" + name
+                        self.write_config(add_to, home + "/.surround/config.yaml", name, path_to_file)
+                        return "File added successfully"
+                    else:
+                        return "No remote named" + add_to
                 else:
                     return "No remote named" + add_to
-            else:
-                return "No remote named" + add_to
+        else:
+            return file_ + " not found"
 
     def pull(self, what_to_pull, file_=None):
         if file_:
