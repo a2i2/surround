721e6bfe7af50b222e4dc3e9065944e1ba5f8236
Jessica Rivera <jessica.riveravillicana@deakin.edu.au>
2019-02-21 15:09:14 +1100

    FIX: Minor corrections in examples main files

--- examples/dump-output/main.py
+++ examples/dump-output/main.py
@@ -3,4 +3,4 @@ import subprocess
 process = subprocess.Popen(['python3', 'examples/dump-output/dump_output.py', '-c=examples/dump-output/config.yaml'])
 process.wait()
 if not (process.returncode is None) and process.returncode > 0:
-    raise Exception('Failed to run sampe file adapter app. Error code: ' + str(process.returncode))
+    raise Exception('Failed to run sample dump output app. Error code: ' + str(process.returncode))
