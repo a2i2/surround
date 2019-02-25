16331ab9160b080c02418d9d7f32157d967092bd
AkshatBajaj <akshat.bajaj@outlook.com>
2019-02-26 10:18:44 +1100

    NEW: Create main remote group to remote cli

--- surround/remote/cli.py
+++ surround/remote/cli.py
@@ -3,5 +3,6 @@ import argparse
 __author__ = 'Akshat Bajaj'
 __date__ = '2019/02/26'
 
-def add_group_to_parser(parser_to_add_to):
-    return parser_to_add_to.add_argument_group('group')
+def create_main_remote_group(parser_to_add_to):
+    remote_group = parser_to_add_to.add_argument_group('group')
+    remote_group.add_argument('--global', help="Used to specify a global remote", action='store_true', dest='glob')
