782814c82c564c6dcccaf1c193ead7bba0487afc
AkshatBajaj <akshat.bajaj@outlook.com>
2019-02-27 10:24:23 +1100

    FIX: Remove remote group from remote cli

--- surround/remote/cli.py
+++ surround/remote/cli.py
@@ -63,11 +63,3 @@ def parse_remote_args(parsed_args):
             else:
                 print("Not a surround project")
                 print("Goto project root directory")
-
-def create_main_remote_group(parser_to_add_to):
-    append_view_remote_group_to_main_remote(parser_to_add_to)
-
-def append_view_remote_group_to_main_remote(remote_parser):
-    view_remote_group = remote_parser.add_argument_group('view-remote-group')
-    view_remote_group.add_argument('--global', help="Used to specify a global remote", action='store_true', dest='glob')
-    view_remote_group.add_argument('-v', '--verify', help="Verify remote", action='store_true')
