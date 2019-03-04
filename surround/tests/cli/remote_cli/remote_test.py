adc273de8f2438ddd03490619a716959c0f33395
AkshatBajaj <akshat.bajaj@outlook.com>
2019-03-04 14:08:39 +1100

    TEST: test_remote_add

--- surround/tests/cli/remote_cli/remote_test.py
+++ surround/tests/cli/remote_cli/remote_test.py
@@ -16,3 +16,12 @@ class RemoteTest(unittest.TestCase):
 
         process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='remote')
         self.assertEqual(process.stdout, "info: no remote found\n")
+
+    def test_remote_add(self):
+        process = subprocess.run(['surround', 'remote', '-a', '-n', 'data', '-u', os.getcwd()], encoding='utf-8', stdout=subprocess.PIPE, cwd='remote')
+
+        process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='remote')
+        self.assertEqual(process.stdout, "data\n")
+
+        process = subprocess.run(['surround', 'remote', '-v'], encoding='utf-8', stdout=subprocess.PIPE, cwd='remote')
+        self.assertEqual(process.stdout, "data: " + os.getcwd() + "\n")
