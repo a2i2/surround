85a214d29d25fd98cfb48b55632786fdc7c5f950
AkshatBajaj <akshat.bajaj@outlook.com>
2019-03-04 13:21:12 +1100

    TEST: Init test rejecting path

--- surround/tests/cli/project_cli/init_test.py
+++ surround/tests/cli/project_cli/init_test.py
@@ -7,6 +7,10 @@ __date__ = '2019/03/01'
 
 class InitTest(unittest.TestCase):
 
-    def test_init(self):
+    def test_happy_path(self):
         process = subprocess.run(['surround', 'init', './', '-p', 'remote', '-d', 'remote'], encoding='utf-8', stdout=subprocess.PIPE)
         self.assertEqual(process.stdout, "Project created at " + os.getcwd() + "/remote\n")
+
+    def test_rejecting_path(self):
+        process = subprocess.run(['surround', 'init', './', '-p', 'remote', '-d', 'remote'], encoding='utf-8', stdout=subprocess.PIPE)
+        self.assertEqual(process.stdout, "Directory ./remote already exists\n")
