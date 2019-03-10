4c405e6ce1aadfe99c0a9273f4a67b62e710afd3
AkshatBajaj <akshat.bajaj@outlook.com>
2019-03-11 10:56:06 +1100

    FIX: Remove unused imports from send_request

--- surround/runner/web/send_request.py
+++ surround/runner/web/send_request.py
@@ -1,22 +1,18 @@
 import tornado.httpclient
-import json
-from tornado.escape import json_decode, json_encode
 from tornado import gen
 import tornado.options
 
-
-@tornado.gen.coroutine  
+@tornado.gen.coroutine
 def json_fetch(http_client, body):
     response = yield http_client.fetch("http://localhost:8889/predict", method='POST', body=body)
     raise gen.Return(response)
 
 @tornado.gen.coroutine
 def request():
-    body = '{"test_json": "ok"}'
+    body = '{"endpoint": "predict"}'
     http_client = tornado.httpclient.AsyncHTTPClient()
     http_response = yield json_fetch(http_client, body)
     print(http_response.body)
-
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
