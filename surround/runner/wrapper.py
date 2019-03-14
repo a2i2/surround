6c689999102bafd4992f603a18debd09bccbc792
AkshatBajaj <akshat.bajaj@outlook.com>
2019-03-15 10:14:50 +1100

    ENHANCE: Validate type of uploaded object

--- surround/runner/wrapper.py
+++ surround/runner/wrapper.py
@@ -1,11 +1,19 @@
-from abc import ABC, abstractmethod
-
-class Wrapper(ABC):
+class Wrapper():
     def __init__(self, surround, type_of_uploaded_object):
         self.surround = surround
         self.type_of_uploaded_object = type_of_uploaded_object
         self.surround.init_stages()
 
-    @abstractmethod
     def run(self, uploaded_data):
-        pass
+        return self.validate
+
+    def validate(self):
+        return self.validate_type_of_uploaded_object()
+
+    def validate_type_of_uploaded_object(self):
+        allowed_types = ['JSON', 'image']
+        for type_ in allowed_types:
+            if self.type_of_uploaded_object == type_:
+                return True
+        print("Selected type not allowed")
+        return False
