cd95c835f3fa6089ed903fb7d63f15b1bf5e04bc
AkshatBajaj <akshat.bajaj@outlook.com>
2019-03-14 16:56:59 +1100

    ENHANCE: Call init stages from wrapper

--- surround/runner/wrapper.py
+++ surround/runner/wrapper.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 class Wrapper(ABC):
     def __init__(self, surround):
         self.surround = surround
+        self.surround.init_stages()
 
     @abstractmethod
     def run(self, uploaded_data):
