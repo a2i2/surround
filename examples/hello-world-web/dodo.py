c432be7137e9f31ff2521c00e64531ee52ddf35f
AkshatBajaj <akshat.bajaj@outlook.com>
2019-03-15 14:51:24 +1100

    FIX: Dodo and classname in example

--- examples/hello-world-web/dodo.py
+++ examples/hello-world-web/dodo.py
@@ -3,5 +3,5 @@ DOIT_CONFIG = {'verbosity':2}
 def task_dev():
     """Run the main task for the project"""
     return {
-        'actions': ["python3 -m hello_world_web"],
+        'actions': ["python3 -m hello-world-web"],
     }
--- examples/hello-world-web/hello-world-web/__main__.py
+++ examples/hello-world-web/hello-world-web/__main__.py
@@ -1,11 +1,11 @@
 import logging
-from .stages import WebWrapper
+from .stages import PipelineWrapper
 
 logging.basicConfig(level=logging.INFO)
 
 def main():
-    wrapper = WebWrapper()
-    wrapper.run()
+    wrapper = PipelineWrapper()
+    wrapper.run(None)
 
 if __name__ == "__main__":
     main()
