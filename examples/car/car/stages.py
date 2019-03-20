11f706b20c88903d8724af6fbbb18b6fae971d40
AkshatBajaj <akshat.bajaj@outlook.com>
2019-03-20 17:02:24 +1100

    ENHANCE: Add ExtractCar stage to car pipeline

--- examples/car/car/stages.py
+++ examples/car/car/stages.py
@@ -1,6 +1,8 @@
 import json
 import subprocess
 import requests
+import cv2
+import numpy as np
 from surround import Stage, SurroundData
 
 class CarData(SurroundData):
@@ -51,3 +53,52 @@ class DetectCar(Stage):
             ocr_url, headers=headers, data=json.dumps(data))
 
         surround_data.output_data = response.json()
+
+class ExtractCar(Stage):
+    def operate(self, surround_data, config):
+        self.crop_image(surround_data)
+
+    def crop_image(self, surround_data):
+        # Read a image
+        I = cv2.imread('data/temp.jpg')
+
+        # Define the polygon coordinates to use or the crop
+        polygon = [[[20,110],[450,108],[340,420],[125,420]]]
+
+        # First find the minX minY maxX and maxY of the polygon
+        minX = I.shape[1]
+        maxX = -1
+        minY = I.shape[0]
+        maxY = -1
+        for point in polygon[0]:
+
+            x = point[0]
+            y = point[1]
+
+            if x < minX:
+                minX = x
+            if x > maxX:
+                maxX = x
+            if y < minY:
+                minY = y
+            if y > maxY:
+                maxY = y
+
+        # Go over the points in the image if they are outside of the enclosing rectangle put zero
+        # if not check if they are inside the polygon or not
+        croppedImage = np.zeros_like(I)
+        for y in range(0,I.shape[0]):
+            for x in range(0, I.shape[1]):
+
+                if x < minX or x > maxX or y < minY or y > maxY:
+                    continue
+
+                if cv2.pointPolygonTest(np.asarray(polygon),(x,y),False) >= 0:
+                    croppedImage[y, x, 0] = I[y, x, 0]
+                    croppedImage[y, x, 1] = I[y, x, 1]
+                    croppedImage[y, x, 2] = I[y, x, 2]
+
+        # Now we can crop again just the envloping rectangle
+        finalImage = croppedImage[minY:maxY,minX:maxX]
+
+        cv2.imwrite('data/temp.jpg',finalImage)
--- examples/car/car/wrapper.py
+++ examples/car/car/wrapper.py
@@ -1,10 +1,10 @@
 import json
 from surround import Surround, Wrapper, AllowedTypes
-from stages import CarData, EncodeImage, DetectCar
+from stages import CarData, EncodeImage, DetectCar, ExtractCar
 
 class PipelineWrapper(Wrapper):
     def __init__(self):
-        surround = Surround([EncodeImage(), DetectCar()], __name__)
+        surround = Surround([EncodeImage(), DetectCar(), ExtractCar()], __name__)
         type_of_uploaded_object = AllowedTypes.IMAGE
         self.config = surround.config
         super().__init__(surround, type_of_uploaded_object)
