a15758c1eac2800505bc8ea565a935aa58b5a839
AkshatBajaj <akshat.bajaj@outlook.com>
2019-03-27 16:16:01 +1100

    ENHANCE: Use add_bucket for aws push and pull

--- surround/remote/aws.py
+++ surround/remote/aws.py
@@ -10,8 +10,13 @@ class AWS(BaseRemote):
         super().__init__()
         self.s3 = boto3.client('s3', aws_access_key_id=AWS.ACCESS_KEY, aws_secret_access_key=AWS.SECRET_KEY)
 
+    def get_bucket(self, path_to_remote):
+        return path_to_remote.split("//")[1]
+
     def pull_file(self, what_to_pull, path_to_remote, relative_path_to_remote_file, path_to_local_file):
-        self.s3.download_file("surround-" + what_to_pull, relative_path_to_remote_file, path_to_local_file)
+        bucket = self.get_bucket(path_to_remote)
+        self.s3.download_file(bucket, relative_path_to_remote_file, path_to_local_file)
 
     def push_file(self, what_to_push, path_to_remote, relative_path_to_remote_file, path_to_local_file):
-        self.s3.upload_file(path_to_local_file, "surround-" + what_to_push, relative_path_to_remote_file)
+        bucket = self.get_bucket(path_to_remote)
+        self.s3.upload_file(path_to_local_file, bucket, relative_path_to_remote_file)
