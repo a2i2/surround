95fa02eb90e7b326b5fdba80b9ed0b9c45a675af
AkshatBajaj <akshat.bajaj@outlook.com>
2019-04-02 10:29:23 +1100

    FIX: Remove print statements for endpoint access and use tornado internal logging instead

--- surround/runner/web/api.py
+++ surround/runner/web/api.py
@@ -10,29 +10,24 @@ START_TIME = datetime.datetime.now()
 
 class HealthCheck(tornado.web.RequestHandler):
     def get(self):
-        print("info: started get request at /")
         self.write(dict(
             app="Surround Server",
             version=pkg_resources.get_distribution("surround").version,
             uptime=str(datetime.datetime.now() - START_TIME)
         ))
-        print("info: finished get request at /")
 
 class Upload(tornado.web.RequestHandler):
     def get_template_path(self):
         return os.getcwd()
 
     def get(self):
-        print("info: started get request at /upload")
         self.render("upload.html")
-        print("info: finished get request at /upload")
 
 class Predict(tornado.web.RequestHandler):
     def initialize(self, wrapper):
         self.wrapper = wrapper
 
     def post(self):
-        print("info: started post request at /predict")
         output = None
         if self.wrapper.type_of_uploaded_object == AllowedTypes.IMAGE:
             fileinfo = self.request.files['data'][0]
@@ -40,7 +35,6 @@ class Predict(tornado.web.RequestHandler):
         else:
             output = self.wrapper.process(self.request.body)
         self.write({"output": output})
-        print("info: finished post request at /predict")
 
 def make_app(wrapper_object):
     predict_init_args = dict(wrapper=wrapper_object)
