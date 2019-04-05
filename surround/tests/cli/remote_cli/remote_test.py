f9f19dd44d53cc467278462234515273ab4e5a66
AkshatBajaj <akshat.bajaj@outlook.com>
2019-04-05 13:12:48 +1100

    FIX: Remove goto project root statement from test_remote

--- surround/tests/cli/remote_cli/remote_test.py
+++ surround/tests/cli/remote_cli/remote_test.py
@@ -9,7 +9,7 @@ class RemoteTest(unittest.TestCase):
 
     def test_remote(self):
         process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE)
-        self.assertEqual(process.stdout, "error: not a surround project\nerror: goto project root directory\n")
+        self.assertEqual(process.stdout, "error: not a surround project\n")
 
         process = subprocess.run(['surround', 'init', './', '-p', 'temp', '-d', 'temp'], encoding='utf-8', stdout=subprocess.PIPE)
         self.assertEqual(process.stdout, "info: project created at " + os.getcwd() + "/temp\n")
