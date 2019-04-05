6d3c5f98f08c035e58f2f212f60f3d87e802d9bc
AkshatBajaj <akshat.bajaj@outlook.com>
2019-04-05 15:48:34 +1100

    TEST: Push from subdir

--- surround/tests/cli/project_cli/surround_project_test.py
+++ surround/tests/cli/project_cli/surround_project_test.py
@@ -76,6 +76,34 @@ class InitTest(unittest.TestCase):
         process = subprocess.run(['surround', 'pull', 'test_remote', '-k', 'a.txt'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
         self.assertEqual(process.stdout, "info: test_remote/a.txt already exists\n")
 
+    def test_push_from_subdir(self):
+        process = subprocess.run(['surround', 'push', 'test_remote', '-k', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE)
+        self.assertEqual(process.stdout, "error: not a surround project\n")
+
+        process = subprocess.run(['surround', 'push', 'test_remote', '-k', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        self.assertEqual(process.stdout, "error: no remote named test_remote\n")
+
+        process = subprocess.run(['surround', 'push', 'test_remote', '-k', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        self.assertEqual(process.stdout, "error: no remote named test_remote\n")
+
+        process = subprocess.run(['surround', 'remote', '-a', '-n', 'test_remote', '-u', 'remote', '-t', 'data'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+
+        process = subprocess.run(['surround', 'push', 'test_remote', '-k', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        self.assertEqual(process.stdout, "error: file does not exist\n")
+
+        process = subprocess.run(['surround', 'push', 'test_remote', '-k', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        self.assertEqual(process.stdout, "error: file does not exist\n")
+
+        process = subprocess.run(['surround', 'push', 'test_remote', '-k', 'a.txt'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        self.assertEqual(process.stdout, "info: a.txt pushed successfully\n")
+
+        process = subprocess.run(['surround', 'push', 'test_remote', '-k', 'a.txt'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        self.assertEqual(process.stdout, "info: remote/temp/a.txt already exists\n")
+
+        process = subprocess.run(['rm', 'test_remote/a.txt'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        process = subprocess.run(['surround', 'pull', 'test_remote', '-k', 'a.txt'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        self.assertEqual(process.stdout, "info: a.txt pulled successfully\n")
+
     def tearDown(self):
         # Remove residual directories and files
         subprocess.run(['rm', '-r', './temp'], encoding='utf-8', stdout=subprocess.PIPE)
