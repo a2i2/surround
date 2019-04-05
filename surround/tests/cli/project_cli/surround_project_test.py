09ddafd964b18247ef88fe5940de04e9f3abba78
AkshatBajaj <akshat.bajaj@outlook.com>
2019-04-05 15:13:20 +1100

    TEST: Add from subdir

--- surround/tests/cli/project_cli/surround_project_test.py
+++ surround/tests/cli/project_cli/surround_project_test.py
@@ -6,6 +6,10 @@ class InitTest(unittest.TestCase):
     def setUp(self):
         subprocess.run(['surround', 'init', './', '-p', 'temp', '-d', 'temp'], encoding='utf-8', stdout=subprocess.PIPE)
 
+        subprocess.run(['mkdir', 'remote'], encoding='utf-8', stdout=subprocess.PIPE)
+        subprocess.run(['mkdir', 'test_remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        subprocess.run(['touch', 'a.txt'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/test_remote')
+
     def test_run_from_subdir(self):
         process = subprocess.run(['surround', 'run'], encoding='utf-8', stdout=subprocess.PIPE)
         self.assertEqual(process.stdout, "error: not a surround project\n")
@@ -30,6 +34,28 @@ class InitTest(unittest.TestCase):
         process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
         self.assertEqual(process.stdout, "test_remote\n")
 
+    def test_add_from_subdir(self):
+        process = subprocess.run(['surround', 'add', 'test_remote', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE)
+        self.assertEqual(process.stdout, "error: not a surround project\n")
+
+        process = subprocess.run(['surround', 'add', 'test_remote', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        self.assertEqual(process.stdout, "error: no remote named test_remote\n")
+
+        process = subprocess.run(['surround', 'add', 'test_remote', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        self.assertEqual(process.stdout, "error: no remote named test_remote\n")
+
+        process = subprocess.run(['surround', 'remote', '-a', '-n', 'test_remote', '-u', 'remote', '-t', 'data'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+
+        process = subprocess.run(['surround', 'add', 'test_remote', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        self.assertEqual(process.stdout, "error: a.jpg not found.\n")
+
+        process = subprocess.run(['surround', 'add', 'test_remote', 'a.jpg'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        self.assertEqual(process.stdout, "error: a.jpg not found.\n")
+
+        process = subprocess.run(['surround', 'add', 'test_remote', 'a.txt'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        self.assertEqual(process.stdout, "info: file added successfully\n")
+
     def tearDown(self):
-        # Remove residual files
+        # Remove residual directories and files
         subprocess.run(['rm', '-r', './temp'], encoding='utf-8', stdout=subprocess.PIPE)
+        subprocess.run(['rm', '-r', './remote'], encoding='utf-8', stdout=subprocess.PIPE)
