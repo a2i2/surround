856cc28f7a3ed00437ab35f030f3a12401933e0a
AkshatBajaj <akshat.bajaj@outlook.com>
2019-04-05 14:08:51 +1100

    TEST: Remote from subdir

--- surround/tests/cli/project_cli/surround_project_test.py
+++ surround/tests/cli/project_cli/surround_project_test.py
@@ -16,6 +16,20 @@ class InitTest(unittest.TestCase):
         process = subprocess.run(['surround', 'run'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
         self.assertEqual(process.stdout[:55], "build    Build the Docker image for the current project")
 
+    def test_remote_from_subdir(self):
+        process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE)
+        self.assertEqual(process.stdout, "error: not a surround project\n")
+
+        process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        self.assertEqual(process.stdout, "info: no remote found\n")
+
+        process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        self.assertEqual(process.stdout, "info: no remote found\n")
+
+        process = subprocess.run(['surround', 'remote', '-a', '-n', 'test_remote', '-u', '~', '-t', 'data'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp/temp')
+        self.assertEqual(process.stdout, "test_remote\n")
+
     def tearDown(self):
         # Remove residual files
         subprocess.run(['rm', '-r', './temp'], encoding='utf-8', stdout=subprocess.PIPE)
