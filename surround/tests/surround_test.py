81d960c2a57d59413f8bc48be66b93d5484ace66
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2019-06-06 13:54:10 +1000

    ENHANCE: Update surround tests and linter to utilise assembler

--- surround/tests/surround_test.py
+++ surround/tests/surround_test.py
@@ -1,13 +1,20 @@
 import unittest
 import os
-from surround import Surround, Stage, SurroundData, Config
-from .stages.first_stage import FirstStage
+from surround import Assembler, Estimator, SurroundData, Config
 
-class HelloStage(Stage):
-    def operate(self, surround_data, config):
-        surround_data.text = "hello"
+
+test_text = "hello"
+
+
+class HelloStage(Estimator):
+    def estimate(self, surround_data, config):
+        surround_data.text = test_text
         if "helloStage" in config:
             surround_data.config_value = config["helloStage"]["suffix"]
+
+    def fit(self, surround_data, config):
+        print("No training implemented")
+
 
 class BasicData(SurroundData):
     text = None
@@ -15,37 +22,28 @@ class BasicData(SurroundData):
     stage1 = None
     stage2 = None
 
+
 class TestSurround(unittest.TestCase):
 
     def test_happy_path(self):
-        surround = Surround([HelloStage()])
         data = BasicData()
-        surround.process(data)
-        self.assertEqual(data.text, "hello")
+        assembler = Assembler("Happy path", data, HelloStage(), Config())
+        assembler.init_assembler()
+        assembler.run()
+        self.assertEqual(data.text, test_text)
 
     def test_rejecting_attributes(self):
-        surround = Surround([HelloStage()])
         data = BasicData()
-        surround.process(data)
+        assembler = Assembler("Reject attribute", data, HelloStage(), Config())
+        assembler.init_assembler()
+        assembler.run()
         self.assertRaises(AttributeError, getattr, data, "no_text")
 
     def test_surround_config(self):
         path = os.path.dirname(__file__)
         config = Config()
         config.read_config_files([os.path.join(path, "config.yaml")])
-        surround = Surround([HelloStage()])
-        surround.set_config(config)
         data = BasicData()
-        surround.process(data)
+        assembler = Assembler("Surround config", data, HelloStage(), config)
+        assembler.run()
         self.assertEqual(data.config_value, "Scott")
-
-    def test_surround_override(self):
-        path = os.path.dirname(__file__)
-        surround = Surround([FirstStage()])
-        config = Config()
-        config.read_config_files([os.path.join(path, "stages.yaml")])
-        surround.set_config(config)
-        data = BasicData()
-        surround.process(data)
-        self.assertEqual(data.stage1, "first stage")
-        self.assertEqual(data.stage2, "second stage")
