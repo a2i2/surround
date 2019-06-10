4dac8be80698d98cd55532fb5e38095265bd1b5e
Zac Brannelly <101668878@student.swin.edu.au>
2019-06-10 10:34:08 +1000

    DOC: Change theme and increase quality of logo
    DOC: Fix image

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,22 +80,19 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {"logo_only": True}
+html_theme_options = {"logo": "../_images/temp_logo_hq.png", "logo_name": False}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "temp_logo.png"
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
