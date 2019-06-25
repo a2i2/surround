73cdad95fecc71e1573808ef6cc86f237940e2f8
Zac Brannelly <101668878@student.swin.edu.au>
2019-06-25 15:34:04 +1000

    ENHANCE: Put package version into surround.__version__

--- surround/__init__.py
+++ surround/__init__.py
@@ -1,6 +1,10 @@
+import pkg_resources
+
 from .surround import SurroundData
 from .stage import Validator, Filter, Estimator
 from .visualiser import Visualiser
 from .config import Config
 from .assembler import Assembler
 from .runners import Runner
+
+__version__ = pkg_resources.get_distribution("surround").version
