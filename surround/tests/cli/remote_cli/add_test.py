fca98ee38daa62f618ddd08b450c54f8e65d14f0
Zac Brannelly <101668878@student.swin.edu.au>
2019-09-20 10:10:04 +1000

    TEST: Test remote add functionality directly via API

---
+++ surround/tests/cli/remote_cli/add_test.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+import unittest
+import subprocess
+
+from surround.remote.local import Local
+
+__author__ = 'Akshat Bajaj'
+__date__ = '2019/03/04'
+
+class AddTest(unittest.TestCase):
+
+    def test_rejecting_path(self):
+        process = subprocess.run(['surround', 'init', './', '-p', 'temp', '-d', 'temp', '-w', 'no'], encoding='utf-8', stdout=subprocess.PIPE)
+        self.assertIn("info: project created at", process.stdout)
+        self.assertIn("temp", process.stdout)
+
+        is_temp = os.path.isdir(os.path.join(os.getcwd() + "/temp"))
+        self.assertEqual(is_temp, True)
+
+        local_remote = Local()
+
+        os.chdir("temp")
+        result = local_remote.add('data', 'temp.jpg')
+        os.chdir("../")
+        self.assertEqual(result, "error: no remote named data")
+
+        process = subprocess.run(['surround', 'remote', '-a', '-n', 'data', '-u', os.getcwd(), '-t', 'data'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+
+        os.chdir("temp")
+        result = local_remote.add('data', 'temp.jpg')
+        os.chdir("../")
+        self.assertEqual(result, "error: temp.jpg not found.")
+
+    def test_happy_path(self):
+        process = subprocess.run(['surround', 'init', os.getcwd(), '-p', 'temp', '-d', 'temp', '-w', 'no'], encoding='utf-8', stdout=subprocess.PIPE)
+        self.assertIn("info: project created at", process.stdout)
+        self.assertIn("temp", process.stdout)
+
+        is_temp = os.path.isdir(os.path.join(os.getcwd() + "/temp"))
+        self.assertEqual(is_temp, True)
+
+        process = subprocess.run(['surround', 'remote', '-a', '-n', 'data', '-u', os.getcwd(), '-t', 'data'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+
+        local_remote = Local()
+
+        os.chdir("temp")
+        result = local_remote.add('data', os.path.join(os.getcwd(), '..', 'surround/remote/base.py'))
+        os.chdir("../")
+
+        self.assertEqual(result, "info: file added successfully")
+
+    def tearDown(self):
+        # Remove residual files
+        shutil.rmtree('temp')
