3e4d4728b568e63264ccd61de66ea0f1eaf82c61
Zac Brannelly <101668878@student.swin.edu.au>
2019-10-02 16:22:38 +1000

    FIX: Update remote unit tests to new command name

--- surround/tests/cli/remote_cli/list_test.py
+++ surround/tests/cli/remote_cli/list_test.py
@@ -15,7 +15,7 @@ class ListTest(unittest.TestCase):
         is_temp = os.path.isdir(os.path.join(os.getcwd() + "/temp"))
         self.assertEqual(is_temp, True)
 
-        process = subprocess.run(['surround', 'list', 'temp'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        process = subprocess.run(['surround', 'store', 'list', 'temp'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
         self.assertEqual(process.stdout, "error: no remote named temp\n")
 
     def test_happy_path(self):
@@ -25,13 +25,13 @@ class ListTest(unittest.TestCase):
         is_temp = os.path.isdir(os.path.join(os.getcwd() + "/temp"))
         self.assertEqual(is_temp, True)
 
-        process = subprocess.run(['surround', 'remote', '-a', '-n', 'temp_remote', '-u', os.getcwd() + '/temp'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        process = subprocess.run(['surround', 'store', 'remote', '-a', '-n', 'temp_remote', '-u', os.getcwd() + '/temp'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
         self.assertEqual(process.stdout, '')
 
-        process = subprocess.run(['surround', 'remote', '-v'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        process = subprocess.run(['surround', 'store', 'remote', '-v'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
         self.assertRegex(process.stdout, 'temp_remote: .*temp\\n')
 
-        process = subprocess.run(['surround', 'list', 'temp_remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        process = subprocess.run(['surround', 'store', 'list', 'temp_remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
         self.assertListEqual(sorted(str(process.stdout).splitlines()), sorted('config.yaml\n__main__.py\n__init__.py\nfile_system_runner.py\nweb_runner.py\nstages'.splitlines()))
 
     def tearDown(self):
--- surround/tests/cli/remote_cli/remote_test.py
+++ surround/tests/cli/remote_cli/remote_test.py
@@ -9,7 +9,7 @@ __date__ = '2019/03/01'
 class RemoteTest(unittest.TestCase):
 
     def test_remote(self):
-        process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE)
+        process = subprocess.run(['surround', 'store', 'remote'], encoding='utf-8', stdout=subprocess.PIPE)
         self.assertEqual(process.stdout, "error: not a surround project\n")
 
         process = subprocess.run(['surround', 'init', './', '-p', 'temp', '-d', 'temp', '-w', 'no'], encoding='utf-8', stdout=subprocess.PIPE)
@@ -18,7 +18,7 @@ class RemoteTest(unittest.TestCase):
         is_temp = os.path.isdir(os.path.join(os.getcwd() + "/temp"))
         self.assertEqual(is_temp, True)
 
-        process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        process = subprocess.run(['surround', 'store', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
         self.assertEqual(process.stdout, "info: no remote found\n")
 
     def test_remote_add(self):
@@ -28,12 +28,12 @@ class RemoteTest(unittest.TestCase):
         is_temp = os.path.isdir(os.path.join(os.getcwd() + "/temp"))
         self.assertEqual(is_temp, True)
 
-        process = subprocess.run(['surround', 'remote', '-a', '-n', 'data', '-u', os.getcwd()], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        process = subprocess.run(['surround', 'store', 'remote', '-a', '-n', 'data', '-u', os.getcwd()], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
 
-        process = subprocess.run(['surround', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        process = subprocess.run(['surround', 'store', 'remote'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
         self.assertEqual(process.stdout, "data\n")
 
-        process = subprocess.run(['surround', 'remote', '-v'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
+        process = subprocess.run(['surround', 'store', 'remote', '-v'], encoding='utf-8', stdout=subprocess.PIPE, cwd='temp')
         self.assertEqual(process.stdout, "data: " + os.getcwd() + "\n")
 
     def tearDown(self):
