6a179e3f370122152eb6efd2cacc629923a14fcb
Zac Brannelly <101668878@student.swin.edu.au>
2021-01-19 11:49:54 +1100

    FIX: Make init-with-stage-data run correctly

---
+++ examples/init-stage-with-data/config.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+from omegaconf import MISSING
+from surround import BaseConfig, config
+
+@config(name="config")
+@dataclass
+class Config(BaseConfig):
+    path_to_hello_world: str = MISSING
